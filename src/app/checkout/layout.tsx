b4be96ce762434cbda319a4faffe5134aabe5e8e
Yago Santos Gigeck <yago0901@gmail.com>
2025-09-12 00:50:23 -0300

    Fix: layout pag sucess

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,22 +1,11 @@
 import Providers from "@/providers";
 import "../globals.css";
-import { ReactNode, Suspense } from "react";
+import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <Suspense fallback={<LoadingComponent />}>
+    <>
       <Providers>{children}</Providers>
-    </Suspense>
-  );
-}
-
-function LoadingComponent() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-primary/5 to-orange-100/20 flex items-center justify-center">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500 mx-auto mb-4"></div>
-        <p>Carregando informações do pagamento...</p>
-      </div>
-    </div>
+    </>
   );
 }
